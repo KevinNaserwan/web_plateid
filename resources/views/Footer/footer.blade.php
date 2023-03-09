851d2eac6f678efa0944e02468df3c608db035ee
Kevin Naserwan <kevinnaserwan@gmail.com>
2023-03-09 23:52:54 +0700

    login

---
+++ resources/views/Footer/footer.blade.php
@@ -0,0 +1,15 @@
+@extends('Main/app')
+@section('konten')
+    <Footer id="footer" class="footer">
+        <div data-aos="zoom-out">
+            <div class="row justify-content-center">
+                <div class=" col-lg-6">
+                    <div class=" row">
+                        <img src="Img/logo.png" alt="">
+                        <h1>PLATEID</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Footer>
+@endsection
--- resources/views/Page/login.blade.php
+++ resources/views/Page/login.blade.php
@@ -1,1 +1,31 @@
 @extends('Main/app')
+@section('konten')
+    <section id="login" class="login">
+        <div data-aos="zoom-out">
+            <div class=" row justify-content-center">
+                <div class="col-lg-6 col-md-12 img-login" data-aos="fade-up" data-aos-delay="100">
+                    <div class="rectangle"></div>
+                    <div class="herotext">
+                        <div class="textblock">
+                            Vehicle license plate recognition for everything
+                        </div>
+                        <div class="textblock2">
+                            <p></p>
+                        </div>
+                    </div>
+                    <div class="minitext">
+                        get to know everything clarify everything
+                    </div>
+                    <div class="logo">
+                        <div class="icon">
+                            <img src="img/logo.png" alt="">
+                        </div>
+                    </div>
+                    <div class="logo-name">
+                        <h1>PLATEID</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
